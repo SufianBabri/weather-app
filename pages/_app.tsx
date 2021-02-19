14218b6e317aab4a4978d6b19ba5db6b21e6bc25
Sufian <sufian.babri@gmail.com>
2021-02-19 20:13:55 +0500

    Removed App.css.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import {library} from '@fortawesome/fontawesome-svg-core';
 import {fab} from '@fortawesome/free-brands-svg-icons';
 import {fas} from '@fortawesome/free-solid-svg-icons';
 import {far} from '@fortawesome/free-regular-svg-icons';
-// import '../styles/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from '../components/common/NavBar';
 

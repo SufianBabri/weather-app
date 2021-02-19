e34eb41932ba3c893a7ae914a7c9f7b85656e8b5
Sufian <sufian.babri@gmail.com>
2021-02-19 18:19:26 +0500

    Some formatting of SearchBox.tsx

--- src/components/common/SearchBox.tsx
+++ src/components/common/SearchBox.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FormControl} from 'react-bootstrap/cjs';
-import {InputGroup} from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap/cjs';
+import { InputGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/cjs/Button';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface Props {
-	width?: number,
+	width?: number;
 	placeholder?: string;
 	searchQuery: string;
 	onChange: (value: string) => void;
@@ -21,7 +21,13 @@ SearchBox.propTypes = {
 	onSubmit: PropTypes.func.isRequired,
 };
 
-export function SearchBox({width, searchQuery, onChange, onSubmit, ...rest}: Props) {
+export function SearchBox({
+	width,
+	searchQuery,
+	onChange,
+	onSubmit,
+	...rest
+}: Props) {
 	function onkeyup(event: any) {
 		if (event.key === 'Enter') {
 			onSubmit();
@@ -30,15 +36,16 @@ export function SearchBox({width, searchQuery, onChange, onSubmit, ...rest}: Pro
 
 	return (
 		<InputGroup className={`w-${width}`}>
-			<FormControl as="input"
-						 {...rest}
-						 value={searchQuery}
-						 onKeyPress={onkeyup}
-						 onChange={(e) => onChange(e.currentTarget.value)} />
+			<FormControl
+				as="input"
+				{...rest}
+				value={searchQuery}
+				onKeyPress={onkeyup}
+				onChange={(e) => onChange(e.currentTarget.value)}
+			/>
 			<InputGroup.Append>
 				<Button variant="light ml-1" onClick={() => onSubmit()}>
-					<FontAwesomeIcon
-						icon="search" />
+					<FontAwesomeIcon icon="search" />
 				</Button>
 			</InputGroup.Append>
 		</InputGroup>

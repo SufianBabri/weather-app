ee2609e65be27616a9673f149826144c94a84506
Sufian <sufian.babri@gmail.com>
2021-02-24 01:33:10 +0500

    Updated all dependencies.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import {AppProps} from 'next/app';
 import React from 'react';
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {fab} from '@fortawesome/free-brands-svg-icons';
 import {fas} from '@fortawesome/free-solid-svg-icons';
-import {far} from '@fortawesome/free-regular-svg-icons';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from '../components/common/NavBar';
 
 export default function App({Component, pageProps}: AppProps) {
-	library.add(fab, fas, far);
+	library.add(fas);
 	return (
 		<React.Fragment>
 			<NavBar />

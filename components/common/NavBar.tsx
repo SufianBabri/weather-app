736adfd9774e3862ce2e08af360d5e0c41cee56f
Sufian <sufian.babri@gmail.com>
2021-02-24 01:47:42 +0500

    Changed nav link solution.

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
 import Navbar from 'react-bootstrap/cjs/Navbar';
 import Nav from 'react-bootstrap/cjs/Nav';
-import NavLink from './NavLink';
-import {useRouter} from 'next/router';
 
 export default function NavBar() {
 	const appName = process.env.NEXT_PUBLIC_NAME;
-	const {pathname} = useRouter();
 
 	return (
 		<Navbar bg="dark" variant="dark">
 			<Navbar.Brand>{appName}</Navbar.Brand>
 			<Nav className="mr-auto">
-				<NavLink path="/" label="Home" pathname={pathname} />
-				<NavLink path="/about" label="About" pathname={pathname} />
+				<Link href="/" passHref>
+					<Nav.Link>
+						Home
+					</Nav.Link>
+				</Link>
+				<Link href="/about" passHref>
+					<Nav.Link>
+						About
+					</Nav.Link>
+				</Link>
 			</Nav>
 		</Navbar>
 	);
